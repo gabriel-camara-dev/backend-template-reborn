5d4136394241e531cc1acd99c36daace2bde2468
Gabriel Camara <galvares439@gmail.com>
2026-04-18 12:22:04 -0300

    fix: reorder import statements in fastify-jwt type definitions

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,6 +1,5 @@
-import { UserRole } from '@/domain/main/enterprise/entities/user-role.ts'
 import '@fastify/jwt'
-
+import { UserRole } from '@/domain/main/enterprise/entities/user-role.ts'
 
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
